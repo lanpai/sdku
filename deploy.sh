449699bdbb391b63dc57293418363c759495434d
lanpai <lan@piyo.cafe>
2019-11-18 10:31:33 -0800

    Added to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-git checkout gh-pages && cp dist/* ./ && git add index.html main.js && git commit -m "Build" && git push origin gh-pages && git checkout master
+npm run build && \
+git checkout gh-pages && \
+cp dist/* ./ && \
+git add index.html main.js && \
+git commit -m "Build" && \
+git push origin gh-pages && \
+git checkout master
