34ca3385d1d7f2284f6f2a7970634c790fb38bfb
lanpai <lan@piyo.cafe>
2019-11-23 22:53:03 -0800

    Fixed sdku-electron.js

--- sdku-electron.js
+++ sdku-electron.js
@@ -1,16 +1,26 @@
 const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
-    let win = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        frame: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+    setTimeout(() => {
+        let win = new BrowserWindow({
+            width: 1280,
+            height: 720,
+            frame: false,
+            title: 'sdku',
+            transparent: true,
+            show: false,
+            backgroundColor: '#00ffffff',
+            webPreferences: {
+                nodeIntegration: true
+            }
+        });
 
-    win.loadFile('dist/index.html');
+        win.loadFile('dist/index.html');
+
+        win.once('ready-to-show', () => {
+            win.show();
+        });
+    }, 500);
 }
 
 app.on('ready', createWindow);
